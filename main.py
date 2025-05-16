815a635d18f8d2408a9bd26314f70645abc39db5
Zugravel Teodor <211989139+zugravelteodor@users.noreply.github.com>
2025-05-16 16:32:30 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+import random
+
+os.system("python -m pip install --upgrade pip")
+os.system("pip install colorama")
+
+from colorama import Fore
+
+pas_list=["123456","password","12345678","qwerty","123456789","12345","1234","111111","1234567","dragon","123123","baseball","abc123","football","monkey","letmein","shadow","master","qwertyuiop","123321","1234567890","michael","654321","superman","1qaz2wsx","7777777","121212","000000","qazwsx","123qwe","trustno1","jordan","jennifer","zxcvbnm","asdfgh","hunter","buster","soccer","harley","batman","andrew","tigger","sunshine","iloveyou","2000","charlie","robert","thomas","hockey","ranger","daniel","starwars","klaster","112233","george","computer","michelle","jessica","pepper","1111","zxcvbn","555555","11111111","131313","freedom","777777","pass","maggie","aaaaaa","ginger","princess","joshua","cheese","amanda","summer","love","ashley","nicole","chelsea","matthew","access","yankees","987654321","dallas","austin","thunder","taylor","matrix","william","corvette","hello","martin","heather","secret","merlin","diamond"]
+
+os.system("clear")
+pas=input("password: ")
+
+attempts = 0
+
+while True:
+
+    a=random.choice(pas_list)
+    
+    if a == pas:
+        print(f"{Fore.GREEN}Password matched: {a} (attempts: {attempts}){Fore.RESET}")
+        attempts += 1
+        break
+
+    else:
+        print(f"{Fore.RED}Password failed to match: {a}{Fore.RESET}")
+        attempts += 1
